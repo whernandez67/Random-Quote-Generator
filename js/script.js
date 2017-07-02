2d8ece193754ce063d37060df2914df9a6fd7388
whernandez67 <hernandez.walter@gmail.com>
2017-07-02 16:47:47 -0400

    Update script.js
    Made some last minute changes to clean up the look and feel of the way it displayed the citation, year and tags.

--- js/script.js
+++ js/script.js
@@ -7,41 +7,41 @@ var red;
 var green;
 var blue;
 
-//Array of objects containing quotes from https://www.goodreads.com/quotes
+//Array of objects containing quotes from https://www.goodreads.com/quotes 
 var quotes = [
-	 {quote: "Perhaps one did not want to be loved so much as to be understood.",
-	  source: "George Orwell",
-	  citation: "",
-	  year: 1984,
-	  tags: "Truth",
+	 {quote: "Perhaps one did not want to be loved so much as to be understood.", 
+	  source: "George Orwell", 
+	  citation: "", 
+	  year: 1984, 
+	  tags: ["Truth"],
 	 },
-	 {quote: "Be yourself; everyone else is already taken.",
-	  source: "Mahatma Gandhi",
-	  citation: "",
-	  year: "",
-	  tags: "Inspirational",
+	 {quote: "Be yourself; everyone else is already taken.", 
+	  source: "Oscar Wilde", 
+	  citation: "", 
+	  year: "", 
+	  tags: [" Inspirational", " Be-yourself", " Honesty", " Inspirational"], 
 	 },
-	 {quote: "Peace begins with a smile..",
-	  source: "Mother Teresa",
-	  citation: "",
-	  year: "",
-	  tags: "Peace",
+	 {quote: "Peace begins with a smile..", 
+	  source: "Mother Teresa", 
+	  citation: "", 
+	  year: "", 
+	  tags: [" Peace", " Inspirational", " Peace", " Smile", " Smiling"],
 	 },
-	 {quote: "Don't Gain The World & Lose Your Soul, Wisdom Is Better Than Silver Or Gold.",
-	  source: "Bob Marley",
-	  citation: "",
-	  year: "",
-	  tags: "Wisdom",
+	 {quote: "Don't Gain The World & Lose Your Soul, Wisdom Is Better Than Silver Or Gold.", 
+	  source: "Bob Marley", 
+	  citation: "", 
+	  year: "", 
+	  tags: [" Peace", " Soul", " Wealth", " Wisdom"],
 	 },
-	 {quote: "The very essence of romance is uncertainty.",
-	  source: "Oscar Wilde",
-	  citation: "The Importance of Being Earnest and Other Plays",
-	  year: "",
-	  tags: "Love",
-	 },
+	 {quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.", 
+	  source: "Martin Luther King Jr.", 
+	  citation: "A Testament of Hope: The Essential Writings and Speeches", 
+	  year: "", 
+	  tags: [" Darkness", " Drive-out", " Hate", " Inspirational", " Light", " Love", " Peace"],
+	 }, 
  ];
 
-//Function when called to generate random colors each time the button is clicked
+//Function when called to generate random colors each time the button is clicked  
 function generateRandomColor() {
 	var rgbColor;
 	red = Math.floor(Math.random() * 256 );
@@ -49,14 +49,14 @@ function generateRandomColor() {
 	blue = Math.floor(Math.random() * 256 );
 	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 	return rgbColor;
-}
+}	
 
 function print(quote){
 	document.getElementById('quote-box').innerHTML = quote;
 }
 
-/* Function to get a random quote from array of objects using the random number returned from
-the random fucntion then using the array splice property passing the start parameters.
+/* Function to get a random quote from array of objects using the random number returned from 
+the random fucntion then using the array splice property passing the start parameters.   	
 */
 function getRandomQuote(){
  var randomQuote = Math.floor( Math.random() * quotes.length );
@@ -68,35 +68,39 @@ function getRandomQuote(){
 	}
 	  return ramdomSplice;
  }
-
-/* Function and stores the returned quote object in a variable the function constructs a string containing
-the different properties of the quote object using the following HTML template: and it doesn't add a for
-a missing citation or a if the year property is missing but displays the final HTML string to the page.
-You can use this JS snippet to accomplish that: document.getElementById('quote-box').innerHTML */
-function printQuote() {
+ 
+/* Function and stores the returned quote object in a variable the function constructs a string containing 
+the different properties of the quote object using the following HTML template: and it doesn't add a for 
+a missing citation or a if the year property is missing but displays the final HTML string to the page. 
+You can use this JS snippet to accomplish that: document.getElementById('quote-box').innerHTML */ 
+function printQuote() { 
  var quotes = getRandomQuote();
  html = '<p class="quote">' + quotes.quote + '</p>';
- html += '<p class="source">' + quotes.source + '</p>';
+
  if (quotes.citation) {
-      html += '<span class="citation">' + quotes.citation + '</span>';
+      var htmlCitation = quotes.citation;
  } else {
-      html += '';
+      htmlCitation = '';
  }
  if (quotes.year) {
-      html += '<span class="year">' + quotes.year + '</span></p>';
+      var htmlYear =  quotes.year;
  } else {
-      html += '';
- }
+      htmlYear = '';	 
+ }  
  if (quotes.tags) {
-      html += '<p>tags: ' + quotes.tags + '</p>';
+      var htmlTags = quotes.tags;
  } else {
-      html += '';
- }
- print(html);
+      htmlTags += '';
+ } 	 
+  html += '<p class="source">' + quotes.source + '<span class="citation">' + htmlCitation + '</span>' + '<span class="citation">' + htmlYear + '</span></p>'; 
+  
+  html += '<p>tags: ' + htmlTags + '</p>';
 
+ print(html);
+ 
 	// Get random color
  generateRandomColor();
-
- //Change the background color using the generate random color function
- document.getElementById('backgroundcolor').style.backgroundColor = generateRandomColor();
-}
+ 	
+ //Change the background color using the generate random color function 
+ document.getElementById('backgroundcolor').style.backgroundColor = generateRandomColor();	
+}	
